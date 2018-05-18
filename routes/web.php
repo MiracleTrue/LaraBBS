507914a2c87c1788c22490f94ec08bdf9ba7eb67
Miracle <120007700@qq.com>
2018-05-18 09:21:56 +0800

    注册 登录

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,3 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/home', 'HomeController@index')->name('home');
