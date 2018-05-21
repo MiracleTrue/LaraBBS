7b1428b3730e83429ab7f3c98bc765e684c5bdf1
Miracle <120007700@qq.com>
2018-05-21 15:37:10 +0800

    创建分类表

---
+++ app/Entity/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+//    /**
+//     * 与模型关联的数据表
+//     *
+//     * @var string
+//     */
+//    protected $table = 'category';
+//
+//    /**
+//     * 可以通过 $primaryKey 属性，重新定义主键字段
+//     * @var string
+//     */
+//    protected $primaryKey = 'category_id';
+
+    protected $fillable = [
+        'name', 'description',
+    ];
+
+    protected $hidden = [
+
+    ];
+}
