9b7073410b573c35162e0f8d2fb1b98b8911684c
Miracle <120007700@qq.com>
2018-05-22 17:28:46 +0800

    editor编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('editor_simditor/css/simditor.css') }}">
+@stop
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('editor_simditor/js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor_simditor/js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor_simditor/js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor_simditor/js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+
+@stop
+
 @section('content')
 
     <div class="container">
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/editor_simditor/js', 'public/editor_simditor/js')
+    .copyDirectory('resources/assets/editor_simditor/css', 'public/editor_simditor/css')
+;
