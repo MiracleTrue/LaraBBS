11590a0dd5a1684fc2f2c1eedf6767bc967ffa67
Miracle <120007700@qq.com>
2018-06-01 08:44:35 +0800

    后台系统

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="/" target="_blank">Miracle</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:120007700@qq.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,5 @@ Route::resource('category', 'CategoryController', ['only' => ['show']]);
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
