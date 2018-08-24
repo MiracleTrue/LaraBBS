92f366be0d1e075c570f800526f674d1323b9e44
Miracle <120007700@qq.com>
2018-08-24 15:06:11 +0800

    接口频率限制

--- config/api.php
+++ config/api.php
@@ -230,4 +230,20 @@ return [
 
     ],
 
+    /*
+     * 接口频率限制
+     */
+    'rate_limits' => [
+        // 访问频率限制，60次数/1分钟
+        'access' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('RATE_LIMITS', 60),
+        ],
+        // 登录相关，10次数/1分钟
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('SIGN_RATE_LIMITS', 10),
+        ],
+    ],
+
 ];
